c7cc1c4143a084c79f504bccca7ee455feadbac9
wagwan piffting <68403205+wagwan-piffting-blud@users.noreply.github.com>
2022-10-09 22:16:07 -0500

    Forgot a period.
    Signed-off-by: wagwan piffting <68403205+wagwan-piffting-blud@users.noreply.github.com>

--- wapp.bat
+++ wapp.bat
@@ -3,9 +3,10 @@
 echo extension=fileinfo >> .\php\php.ini
 echo post_max_size = 100M >> .\php\php.ini
 echo memory_limit = 1G >> .\php\php.ini
+
 echo LoadModule speling_module modules/mod_speling.so >> .\apache2\conf\httpd.conf
 
 rmdir /s /q .\apache2\htdocs
 mkdir .\apache2\htdocs
 
-robocopy /S ccidb_source \apache2\htdocs
+robocopy /S ccidb_source .\apache2\htdocs
